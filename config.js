9abef098336bac3de6bffa04ef3cfbb5d8d52980
arlein015 <rachehua45@gmail.com>
2026-03-09 00:19:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,24 @@
-// Configuration Threndly
 const CONFIG = {
     firebase: {
+        apiKey: "TON_API_KEY",
+        authDomain: "trendly-fca7c.firebaseapp.com",
         databaseURL: "https://trendly-fca7c-default-rtdb.firebaseio.com",
-        projectId: "trendly-fca7c"
+        projectId: "trendly-fca7c",
+        storageBucket: "trendly-fca7c.appspot.com",
+        messagingSenderId: "TON_ID",
+        appId: "TON_APP_ID"
     },
     emailjs: {
-        publicKey: "TON_ID_PUBLIC", // À copier depuis EmailJS
-        serviceId: "TON_SERVICE_ID",
-        templateId: "TON_TEMPLATE_ID"
+        publicKey: "TON_PUBLIC_KEY",
+        serviceId: "service_id",
+        templateId: "template_id"
     }
 };
 
-// Initialisation automatique
-firebase.initializeApp(CONFIG.firebase);
+// Initialisation
+if (!firebase.apps.length) {
+    firebase.initializeApp(CONFIG.firebase);
+}
 const db = firebase.database();
+const auth = firebase.auth();
 emailjs.init(CONFIG.emailjs.publicKey);
