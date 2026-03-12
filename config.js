52d0487ea7f30476426d525c27f675d7c2aba15a
arlein015 <rachehua45@gmail.com>
2026-03-12 21:55:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const CONFIG = {
-    APP_NAME: "Threndly",
-    APP_VERSION: "1.0.4",
-    THEME_COLOR: "#8a2be2",
-    VERIFIED_COLOR: "#007bff",
-    API_URL: "https://api.threndly.com/v1",
+    APP_NAME: "Floy",
+    APP_VERSION: "1.0.5",
+    // Nouvelles couleurs basées sur ton logo
+    THEME_COLOR_ORANGE: "#ff8c00", // L'orange vif de ton logo
+    VERIFIED_COLOR_BLUE: "#0000cd", // Le bleu électrique de ton logo
+    TEXT_COLOR_DARK_GRAY: "#2f4f4f", // Le gris foncé pour les éléments inactifs
+    API_URL: "https://api.floy.app/v1",
     LOGGED_IN: true,
     USER_DATA: {
         username: "@createur_floy",
